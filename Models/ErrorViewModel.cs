43efc79494c6a8eeb9bf57ce4f71d53a190d9f4f
sergiocortes-dll <contacto.sergiocortes@gmail.com>
2025-10-17 07:02:02 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace appointment_backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
