0e820ddd64c2f1c83d6ca3abc7e37406a026ada7
Farg0n <miguelanga1604@gmail.com>
2025-10-17 11:21:08 -0500

    feat: try catch & http attribute

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -17,34 +17,67 @@ public class PatientController : Controller
     //Este me trae todos los pacientes
     
     //METALE ASYNC
-    public IActionResult Index()
+    //farag0n -listo ya tiene async
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        var patient = _context.Patients.ToList();
-        return View(patient);
+        try
+        {
+            var patients = await _context.Patients.ToListAsync();
+            return View(patients);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ocurrio un error inesperado al traer los pacientes\nError: {e}");
+            ViewBag.ErrorMessage = "No se pudo cargar la lista de pacientes recargue la pagina";
+            return View(Index)
+        }
     }
     
     //Registrar un paciente
     //Si el modelo no es valido, vuelve a mostrar la vista del formulario con los mismos datos que el usuario ingreso.
+    [HttpPost]
     public async Task<IActionResult> Register(Patient patient)
     {
         if (!ModelState.IsValid) return View(patient); 
         if (!AgeValid(patient)) return View(patient);
-        
-        _context.Patients.Add(patient);
-        await _context.SaveChangesAsync();
+
+        try
+        {
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine($"El paciente no se ha podido guardar el la base de datos\nError{e}");
+            ViewBag.ErrorMessage = "No se pudo registrar el paciente, intente de nuevo";
+            return View(patient)
+        }
         return RedirectToAction("Index");
     }
     
     //Eliminar un paciente
+    [HttpPost]
     public async Task<IActionResult> Delete(int id)
     {
         var delete = await _context.Patients.FindAsync(id);
-        _context.Patients.Remove(delete);
-        await _context.SaveChangesAsync();
+        try
+        {
+            _context.Patients.Remove(delete);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine($"El paciente no se pudo eliminar correctamente{e}");
+            ViewBag.ErrorMessage = "Ocurrio un error eliminando el paciente, vuelva a intentarlo mas tarde";
+        }
         return RedirectToAction("Index");
     }
     
     //Este busca el paciente por el Id para poder editarlo
+    [HttpPost]
     public IActionResult Edit(int id)
     {
         var patient = _context.Patients.Find(id);
@@ -57,16 +90,28 @@ public class PatientController : Controller
     }
     
     //Este guarda los cambios al editar un paciente
+    [HttpPost]
     public async Task<IActionResult> SaveEdit(Patient patient)
     {
         if (!ModelState.IsValid) return View(patient);
         if (!AgeValid(patient)) return View(patient);
-        _context.Patients.Update(patient);
-        await _context.SaveChangesAsync();
+        try
+        {
+            _context.Patients.Update(patient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine($"No se pudo guardar la nueva informacion del paciente{e}");
+            ViewBag.ErrorMessage = "El paciente no se pudo editar correctamente";
+            return RedirectToAction("Index");
+        }
         return RedirectToAction("Index");
     }
 
     //Validar que la esta este dentro del rango del 1 al 100
+    [HttpPost]
     public bool AgeValid(Patient patient)
     {
         if (patient.Age < 1 || patient.Age > 100)
